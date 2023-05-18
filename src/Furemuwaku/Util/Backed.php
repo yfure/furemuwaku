d4cafb862af72df5fd44907ecadf0f718590747e
hxAri <ari160824@gmail.com>
2023-05-19 00:11:14 +0700

    Update

---
+++ src/Furemuwaku/Util/Backed.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Yume\Fure\Util;
+
+/*
+ * Backed
+ *
+ * This trait only usage for Enum.
+ *
+ * @package Yume\Fure\Util
+ */
+trait Backed
+{
+	
+	/*
+	 * Mapping enums.
+	 *
+	 * @access Public Static
+	 *
+	 * @params Closure $callback
+	 *
+	 * @return Array
+	 */
+	public static function map( Closure $callback ): Array
+	{
+		return( Arrays::map( array_combine( self::names(), self::cases() ), $callback ) );
+	}
+	
+	/*
+	 * Return enum unit cases.
+	 *
+	 * @access Public Static
+	 *
+	 * @return Array
+	 */
+	public static function names(): Array
+	{
+		return( array_column( self::cases(), "name" ) );
+	}
+	
+	/*
+	 * Return enum backed cases.
+	 *
+	 * @access Public Static
+	 *
+	 * @return Array
+	 */
+	public static function values(): Array
+	{
+		return( array_column( self::cases(), "value" ) );
+	}
+	
+	/*
+	 * Return enum case combined unit with backed.
+	 *
+	 * @access Public Static
+	 *
+	 * @params Bool $reverse
+	 *
+	 * @return Array
+	 */
+	public static function array( Bool $reverse = False ): array
+	{
+		if( $reverse )
+		{
+			return( array_combine( self::values(), self::names() ) );
+		}
+		return( array_combine( self::names(), self::values() ) );
+	}
+	
+}
+
+?>
