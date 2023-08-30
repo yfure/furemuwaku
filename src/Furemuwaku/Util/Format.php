f6672cb66d4cba565eff020cb457a19a1bdeee13
hxAri <hxari@proton.me>
2023-08-30 23:44:34 +0700

    Update

--- src/Furemuwaku/Util/Format.php
+++ src/Furemuwaku/Util/Format.php
@@ -8,84 +8,12 @@ use Yume\Fure\Error;
 
 /*
  * Format
- *
+ * 
  * @package Yume\Fure\Util
  */
 trait Format
 {
-	
-	/*
-	 * Pattern for match iteration syntax.
-	 *
-	 * @access Private Static
-	 *
-	 * @values String
-	 */
-	private static String $iterate = "(?<iterate>(\+|\-){1,2})";
-	
-	/*
-	 * Pattern for match curly syntax.
-	 *
-	 * @access Private Static
-	 *
-	 * @values String
-	 */
-	private static String $curly = "(?<curly>(?:\\\*)\{[\s\t]*(?:\\\*)\})";
-	
-	/*
-	 * Pattern for match array syntax.
-	 *
-	 * @access Private Static
-	 *
-	 * @values String
-	 */
-	private static String $array = "(?<array>(?:[a-zA-Z0-9_\x80-\xff](?:[a-zA-Z0-9_\.\x80-\xff]{0,}[a-zA-Z0-9_\x80-\xff]{1})*)*(?:\\[[^\\[\\]]+\\]|[a-zA-Z0-9_\x80-\xff](?:[a-zA-Z0-9_\.\x80-\xff]{0,}[a-zA-Z0-9_\x80-\xff]{1})*)+(?:\\.[a-zA-Z0-9_\x80-\xff](?:[a-zA-Z0-9_\.\x80-\xff]{0,}[a-zA-Z0-9_\x80-\xff]{1})*|\\[[^\\[\\]]+\\])*)";
-	
-	/*
-	 * Pattern for match static method syntax.
-	 *
-	 * @access Private Static
-	 *
-	 * @values String
-	 */
-	private static String $method = "(?<method>(?<class>[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*(?:\\\[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)*)\:\:(?<method_name>[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*))";
-	
-	/*
-	 * Pattern for match callback syntax.
-	 *
-	 * @access Private Static
-	 *
-	 * @values String
-	 */
-	private static String $callback = "(?<callback>[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*)";
-	
-	/*
-	 * Pattern for match function/ method argument syntax.
-	 *
-	 * @access Private Static
-	 *
-	 * @values String
-	 */
-	private static String $argument = "(?<argument>(?:[\s\t]*)\((?:[\s\t]*)(?<values>%1\$s|%2\$s|%3\$s)(?:[\s\t]*)\))";
-	
-	/*
-	 * Pattern for match function syntax.
-	 *
-	 * @access Private Static
-	 *
-	 * @values String
-	 */
-	private static String $function = "(?<function>(?<function_name>[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*))";
-	
-	/*
-	 * Combined patterns syntax.
-	 *
-	 * @access Private Static
-	 *
-	 * @values String
-	 */
-	private static ? String $combine = Null;
-	
+
 	/*
 	 * String formater.
 	 *
@@ -98,10 +26,24 @@ trait Format
 	 *
 	 * @access Public Static
 	 *
-	 * @syntax {}
-	 * @syntax {+|++|-|--}
-	 * @syntax {[a-zA-Z0-9]+}
-	 * @syntax {Function|Method(?)}
+	 * @syntax default
+	 *  format( "{}", ...$values )
+	 * @syntax iteration
+	 *  format( "{+}", ...$values )
+	 *  format( "{-}", ...$values )
+	 * @syntax index|key
+	 *  format( "{k}", ...$values )
+	 *  format( "{k.v}", ...$values )
+	 *  format( "{k.v[i]}", ...$values )
+	 *  format( "{k.v[i].k}", ...$values )
+	 *  format( "{k.v[i].k[i]}", ...$values )
+	 * @syntax function|method
+	 *  format( "{test(?)}", ...$values )
+	 *  format( "{Test\Test::test(?)}", ...$values )
+	 * @syntax callback
+	 *  format( "{+:lower}", ...$values )
+	 *  format( "{k:lower}", ...$values )
+	 *  format( "{f(?):lower}", ...$values )
 	 *
 	 * @params String $format
 	 *  The raw string to be formatted.
@@ -112,174 +54,172 @@ trait Format
 	 */
 	public static function format( String $format, Mixed ...$values ): String
 	{
-		// Check if pattern syntax has not combine.
-		if( self::$combine === Null )
-		{
-			// Create combine pattern syntax.
-			self::$combine = sprintf( "/^(?:(?<matched>(%1\$s|%2\$s|%3\$s|(?<func>(?:\\\*)(?:%5\$s|%6\$s)%7\$s))(\:(%4\$s))*)(?<skip>\#[^\n]*)*)$/iJ", ...[
-				self::$curly,
-				self::$iterate,
-				self::$array,
-				self::$callback,
-				self::$function,
-				self::$method,
-				sprintf( self::$argument, ...[
-					self::$curly,
-					self::$iterate,
-					self::$array
-				])
-			]);
-		}
-		
-		// Replacing formats.
-		$replace = preg_replace_callback( "/(?:(?<matched>(?<!\\\)\{(?:[\s\t]*)(?<syntax>.*?)(?:[\s\t]*)(?<!\\\)\}))/", subject: $format, callback: function( Array $match ) use( &$values )
-		{
-			// Statically variable.
-			static $i = 0;
-			
-			// Extract variables.
-			$matched = $match['matched'];
-			$syntax = $match['syntax'];
-			
-			try
-			{
-				// Check if syntax is valid.
-				if( preg_match( self::$combine, $syntax, $match, PREG_UNMATCHED_AS_NULL ) )
+		// Return normalized formated string.
+		return( self::formatNormalize(
+			preg_replace_callback(
+
+				// Pattern for capture format syntax.
+				pattern: "/(?<matched>(?<!\\\)\{(?<syntax>.*?)(?<!\\\)\})/ms",
+
+				// String will be format.
+				subject: $format,
+				/*
+				 * Call the format handler.
+				 * 
+				 * @params Array $match
+				 * 
+				 * @return String
+				 */
+				callback: function( Array $match ) use( $values ): String
 				{
-					// Get format values.
-					$value = self::formatValue( $match, $values, $i );
-					
-					// Check if syntax function is exists.
-					if( isset( $match['func'] ) )
-					{
-						// Check if values is not Array type.
-						if( is_array( $value ) === False ) $value = [ $value ];
-						
-						// Get function/ method return values.
-						$value = call_user_func_array( $match['method'] ?? $match['function'], $value );
-					}
-					
-					// Check if method is available.
-					if( isset( $match['callback'] ) )
-					{
-						// Avoid argument error when the value is not String type.
-						$value = Strings::parse( $value );
-						
-						// Matching supported method.
-						return( match( @strtolower( $match['callback'] ) )
-						{
-							// Supported methods.
-							"b64decode",
-							"base64_decode" => @base64_decode( $value ),
-							"b64encode",
-							"base64_encode" => @base64_encode( $value ),
-							"bin2hex" => @bin2hex( $value ),
-							"lcfirst" => @lcfirst( $value ),
-							"lower" => @strtolower( $value ),
-							"ucfirst" => @ucfirst( $value ),
-							"upper" => @strtoupper( $value ),
-							"htmlspecialchars" => @htmlspecialchars( $value ),
-							
-							// Supported hash methods.
-							"md2",
-							"md4",
-							"md5",
-							"sha1",
-							"sha224",
-							"sha256",
-							"sha384",
-							"sha512x2f224",
-							"sha512x2f256",
-							"sha512",
-							"sha3x2d224",
-							"sha3x2d256",
-							"sha3x2d384",
-							"sha3x2d512",
-							"ripemd128",
-							"ripemd160",
-							"ripemd256",
-							"ripemd320",
-							"whirlpool",
-							"tiger128x2c3",
-							"tiger160x2c3",
-							"tiger192x2c3",
-							"tiger128x2c4",
-							"tiger160x2c4",
-							"tiger192x2c4",
-							"snefru",
-							"snefru256",
-							"gost",
-							"gostx2dcrypto",
-							"adler32",
-							"crc32",
-							"crc32b",
-							"crc32c",
-							"fnv132",
-							"fnv1a32",
-							"fnv164",
-							"fnv1a64",
-							"joaat",
-							"murmur3a",
-							"murmur3c",
-							"murmur3f",
-							"xxh32",
-							"xxh64",
-							"xxh3",
-							"xxh128",
-							"haval128x2c3",
-							"haval160x2c3",
-							"haval192x2c3",
-							"haval224x2c3",
-							"haval256x2c3",
-							"haval128x2c4",
-							"haval160x2c4",
-							"haval192x2c4",
-							"haval224x2c4",
-							"haval256x2c4",
-							"haval128x2c5",
-							"haval160x2c5",
-							"haval192x2c5",
-							"haval224x2c5",
-							"haval256x2c5" => @hash( preg_replace_callback( "/x([a-fA-F0-9]{2})/", fn( Array $m ) => @hex2bin( $m[1] ), $match['callback'] ), $value ),
-							
-							// When unsupported callback passed.
-							default => sprintf( "#[value(Unsuported callback %s)]", $match['callback'] )
-						});
-					}
-					return( Strings::parse( $value ) );
-				}
-				else {
-					return( Strings::parse( self::formatValue( [ "matched" => $matched ], $values, $i ) ) );
-				}
-			}
-			catch( Throwable $e )
-			{
-				return( sprintf( "#[%s(%s on line %d in file %s)]", $e::class, $e->getMessage(), $e->getLine(), /**path(*/ $e->getFile()/*, True )*/ ) );
-			}
-			$i++;
-		});
-		return( preg_replace_callback( pattern: "/(?:(?<backslash>\\\{1,})(?<curly>\{|\}))/ms", subject: $replace, callback: fn( Array $match ) => sprintf( "%s%s", $match['backslash'] === "\x5c" ? "" : str_repeat( "\x5c", strlen( $match['backslash'] ) -1 ), $match['curly'] ) ) );
+					// Statically variable.
+					static $i = 0;
+					return( self::formatHandler( $match, $values, $i ) );
+				}),
+		));
 	}
-	
+
 	/*
-	 * Function for get value by match.
-	 *
+	 * Format handler.
+	 * 
+	 * @access Private Static
+	 * 
 	 * @params Array $match
-	 * @params Array $values
-	 * @params Int $i
+	 * @params Array<Int|String,Mixed> &$values
+	 * @params Int &$i
 	 *
+	 * @return String
+	 */
+	private static function formatHandler( Array $match, Array &$values, Int $i ): String
+	{
+		// Patterns matcher.
+		$array = "(?<array>(?:[a-zA-Z0-9_\x80-\xff](?:[a-zA-Z0-9_\.\x80-\xff]{0,}[a-zA-Z0-9_\x80-\xff]{1})*)*(?:\\[[^\\[\\]]+\\]|[a-zA-Z0-9_\x80-\xff](?:[a-zA-Z0-9_\.\x80-\xff]{0,}[a-zA-Z0-9_\x80-\xff]{1})*)+(?:\\.[a-zA-Z0-9_\x80-\xff](?:[a-zA-Z0-9_\.\x80-\xff]{0,}[a-zA-Z0-9_\x80-\xff]{1})*|\\[[^\\[\\]]+\\])*)";
+		$curly = "(?<curly>\{\})";
+		$iterator = "(?<iterator>(\\\x2b{1,2}|\\\x2d{1,2}))";
+		$argument = "(?<argument>\((?:{$array}|{$curly}|{$iterator})*\))";
+		$callback = "(?<callback>[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*(?:\\\[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)*)";
+		$function = "(?<function>[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*(?:\\\[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)*)";
+		$statical = "(?<statical>(?<class>[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*(?:\\\[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)*)\:{2}(?<method>[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*))";
+		$closure = "(?<closure>\\\{0,1}(?:{$function}|{$statical}){$argument})";
+
+		// Pattern for validate syntax.
+		$pattern = "/^(?:{$array}|{$curly}|{$iterator}|{$closure})(?:\:\\\{0,1}{$callback})*$/Jms";
+
+		// Extract variables.
+		$matched = self::formatNormalize( trim( $match['matched'] ) );
+		$syntax = self::formatNormalize( trim( $match['syntax'] ) );
+		$value = "";
+
+		try
+		{
+			// Check if format syntax is valid.
+			if( preg_match( $pattern, str_replace( [ "\n", "\x20" ], "", $syntax ), $match, PREG_UNMATCHED_AS_NULL ) )
+			{
+				// Get format values.
+				$value = self::formatValue( $match, $values, $i );
+
+				// Check if syntax function is exists.
+				if( isset( $match['closure'] ) )
+				{	
+					// Get closure function name.
+					$closure = $match['statical'] ?? $match['function'];
+
+					// Normalize closure parameter.
+					$params = is_array( $value ) ? $value : [$value];
+
+					// Get function/ method return values.
+					$value = call_user_func_array( $closure, $params );
+				}
+
+				// Avoid argument error when the value is not String type.
+				$value = Strings::parse( $value );
+
+				// Check if method is available.
+				if( isset( $match['callback'] ) )
+				{
+					// Normalize value with callback function.
+					$value = match( strtolower( $match['callback'] ) )
+					{
+						// Supported shorthands callback.
+						"b64decode" => @base64_decode( $value ),
+						"b64encode" => @base64_encode( $value ),
+						"bin2hex" => @bin2hex( $value ),
+						"lcfirst" => @lcfirst( $value ),
+						"lower" => @strtolower( $value ),
+						"ucfirst" => @ucfirst( $value ),
+						"upper" => @strtoupper( $value ),
+						"htmlspecialchars" => @htmlspecialchars( $value ),
+
+						// Handle unsupported callback.
+						default => match( True )
+						{
+							// When the callback doesn't supported,
+							// We will check if callback is callable.
+							is_callable( $match['callback'] ) => Strings::parse(
+								call_user_func( $match['callback'], $value )
+							),
+
+							// When the callback is invalid.
+							default => "#[Unsuported callback `{$match['callback']}`]"
+						}
+					};
+				}
+			}
+			else {
+				$value = Strings::parse(
+					self::formatValue( i: $i, values: $values, match: [
+						"matched" => $matched
+					])
+				);
+			}
+		}
+		catch( Throwable $e )
+		{
+			$value = sprintf( "\x23\x5b\x25\x73\x28\x20\x25\x73\x20\x29\x5d", $e::class, $e->getMessage() );
+		}
+		return( $value );
+	}
+
+	/*
+	 * Normalize string with syntax format.
+	 * 
+	 * @access Public Static
+	 * 
+	 * @params String $string
+	 * 
+	 * @return String
+	 */
+	public static function formatNormalize( String $string ): String
+	{
+		return( preg_replace_callback( pattern: "/(?:(?<backslash>\\\{1,})(?<curly>\{|\}))/ms", subject: $string, callback: fn( Array $match ) => sprintf( "%s%s", $match['backslash'] === "\x5c" ? "" : str_repeat( "\x5c", strlen( $match['backslash'] ) -1 ), $match['curly'] ) ) );
+	}
+
+	/*
+	 * Normalize format value.
+	 * 
+	 * @access Private Static
+	 * 
+	 * @params Array<String,String> $match
+	 * @params Array<Int|String,Mixed> $values
+	 * @params Int &$i
+	 * 
 	 * @return Mixed
+	 * 
+	 * @throws Yume\Fure\Error\IndexError
+	 *  When the value of index is not available.
+	 * @throws Yume\Fure\Error\SyntaxError
+	 *  When the syntax is invalid or doesn't supported.
 	 */
 	private static function formatValue( Array $match, Array $values, Int &$i ): Mixed
 	{
 		// Get values form format parameter by key name.
 		if( isset( $match['array'] ) ) return( Arrays::ify( $match['array'], $values ) );
-		
+			
 		// Check if matched by iteration symbols.
-		if( isset( $match['iterate'] ) )
+		if( isset( $match['iterator'] ) )
 		{
 			// Get current index by symbol.
-			$index = $match['iterate'][0] === "\x2b" ? $i++ : $i--;
+			$index = $match['iterator'][0] === "\x2b" ? $i++ : $i--;
 			
 			// Check if index by iteration is exists.
 			if( isset( $values[$index] ) )
@@ -297,11 +237,15 @@ trait Format
 			{
 				return( $values[$i++] );
 			}
+			var_dump([
+				$match,
+				$values
+			]);
 			throw new Error\IndexError( $i++ );
 		}
 		throw new Error\SyntaxError( sprintf( "Unsupported syntax %s in string formater", $match['matched'] ) );
 	}
-	
+
 }
 
 ?>
