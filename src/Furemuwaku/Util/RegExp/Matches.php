e6a42ec9d17b65515e97001cebbad6c5bc125218
hxAri <hxari@proton.me>
2024-02-05 21:24:27 +0700

    Updated

--- src/Furemuwaku/Util/RegExp/Matches.php
+++ src/Furemuwaku/Util/RegExp/Matches.php
@@ -11,14 +11,11 @@ use Yume\Fure\Util\Arr;
  */
 final class Matches extends Arr\Lists {
 	
-	public Readonly Int $lastPosition;
-	
 	/*
 	 * @inherit Yume\Fure\Util\Arr\Lists
 	 *
 	 */
-	public function __construct( Array $matches, public Readonly Arr\Associative $groups, public Readonly Int $position ) {
-		$this->lastPosition = $position + strlen( $matches[0] );
+	public function __construct( Array $matches, public Readonly Arr\Associative $groups, public Readonly Int $position, public Readonly Int $lastPosition ) {
 		parent::__construct( $matches );
 	}
 	
